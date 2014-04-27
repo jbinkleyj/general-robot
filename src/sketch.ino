893904738908a3fc9c93d235847d225097d07b7f
Joao Salavisa <joao.salavisa@gmail.com>
2014-04-27 23:04:09 +0000

    cleanup

--- src/sketch.ino
+++ src/sketch.ino
@@ -1,5 +1,23 @@
+/*
+   Generic Robot with obstacle avoidance capabilities
+   Copyright (C) 2014  Joao Salavisa (joao.salavisa@gmail.com)
+
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+
 #include <Servo.h>
-#include <math.h>
 #include "Scanner.h"
 #include "HcSr04.h"
 
@@ -85,6 +103,7 @@ void loop() {
 
 }
 
+// Isolate this crappy code
 void motorAOn()
 {
     digitalWrite(ENABLE_A, HIGH);
